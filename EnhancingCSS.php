f85290b993c418d236b9ed9ccda63f1be835da1d
miyauchi <miyauchi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-11 10:58:29 +0000

    up to 0.2
    git-svn-id: https://plugins.svn.wordpress.org/enhancing-css/tags/0.2@288063 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- EnhancingCSS.php
+++ EnhancingCSS.php
@@ -4,7 +4,7 @@ Plugin Name: Enhancing CSS
 Plugin URI: http://firegoby.theta.ne.jp/wp/enhancingcss
 Description: Add & Edit custom stylesheet throught WordPress Dashboard.
 Author: Takayuki Miyauchi (THETA NETWORKS Co,.Ltd)
-Version: 0.1
+Version: 0.2
 Author URI: http://firegoby.theta.ne.jp/
 */
 
