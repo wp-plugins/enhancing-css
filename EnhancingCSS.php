c0f6bd2f7b731529ed43fefe8f36a164d0f79d48
miyauchi <miyauchi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-09 16:31:14 +0000

    tagging 0.7
    git-svn-id: https://plugins.svn.wordpress.org/enhancing-css/tags/0.7@320988 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- EnhancingCSS.php
+++ EnhancingCSS.php
@@ -4,7 +4,7 @@ Plugin Name: Enhancing CSS
 Plugin URI: http://firegoby.theta.ne.jp/wp/enhancingcss
 Description: Add & Edit custom stylesheet throught WordPress Dashboard.
 Author: Takayuki Miyauchi (THETA NETWORKS Co,.Ltd)
-Version: 0.3
+Version: 0.7
 Author URI: http://firegoby.theta.ne.jp/
 */
 
@@ -52,16 +52,16 @@ class EnhancingCSS{
         $this->basedir = WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__));
         add_action('admin_menu', array(&$this, 'add_admin_page'));
         add_action('admin_head', array(&$this, 'admin_head'));
-        add_action('wp_head', array(&$this, 'wp_head'));
+        add_action('wp_head', array(&$this, 'wp_head'), 9999);
     }
 
     private function get_style_url()
     {
         global $wp_rewrite;
         if ($wp_rewrite->using_permalinks()) {
-            $url = site_url().'/'.$this->name.'.css';
+            $url = get_bloginfo('url').'/'.$this->name.'.css';
         } else {
-            $url = site_url().'/?'.$this->name.'=true';
+            $url = get_bloginfo('url').'/?'.$this->name.'=true';
         }
         return $url;
     }
@@ -160,6 +160,8 @@ class EnhancingCSS{
             $files[] = $this->get_style_url();
             $files = array_map('trim', $files);
             return join(",", $files);
+        } else {
+            return $css;
         }
     }
 
